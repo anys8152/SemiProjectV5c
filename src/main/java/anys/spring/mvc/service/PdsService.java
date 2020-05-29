f88266c73b7868e0832894acb413561a234232e0
anys8152 <64997390+anys8152@users.noreply.github.com>
2020-05-29 11:57:52 +0900

    파일 업로드 기능 보완

--- src/main/java/anys/spring/mvc/service/PdsService.java
+++ src/main/java/anys/spring/mvc/service/PdsService.java
@@ -23,9 +23,9 @@ public class PdsService {
 
         // 첨부파일 정보를 임의로 저장
         /*pd.setFname("anbn.zip");*/
-        pd.setFsize("1234");
-        pd.setFdown("970");
-        pd.setFtype("zip");
+        /*pd.setFsize("1234");*/
+        pd.setFdown("0");
+        /*pd.setFtype("zip");*/
 
         if (pdao.insertPds(pd))
             result = "데이터 입력 성공!!";
